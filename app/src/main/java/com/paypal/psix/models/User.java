6c0cad2b3a52b1199d61178175da239df5e7e136
Shay Davidson <shay.h.davidson@gmail.com>
2015-03-03 12:09:44 +0200

    Added ActiveAndroid and models.

---
+++ app/src/main/java/com/paypal/psix/models/User.java
@@ -0,0 +1,35 @@
+package com.paypal.psix.models;
+
+import com.activeandroid.Model;
+import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Table;
+
+import java.util.List;
+
+@Table(name = "Users")
+public class User extends Model {
+
+    @Column(name = "FbUserId", unique = true, onUniqueConflict = Column.ConflictAction.FAIL)
+    public String fbUserId;
+
+    @Column(name = "Name")
+    public String name;
+
+    public List<Event> events() {
+        return getMany(Event.class, "Organizer");
+    }
+
+    public List<Rsvp> rsvps() {
+        return getMany(Rsvp.class, "User");
+    }
+
+    public User() {
+        super();
+    }
+
+    public User(String fbUserId, String name) {
+        super();
+        this.fbUserId = fbUserId;
+        this.name = name;
+    }
+}
