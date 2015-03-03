5a882492fec3e4f13c9bc0a45d34ecec011dc5e1
Benny Weingarten-Gabbay <gardenofwine@gmail.com>
2015-03-03 15:02:53 +0200

    Add Setup Event page (initial)

---
+++ app/src/main/java/com/paypal/psix/SetupEventActivity.java
@@ -0,0 +1,16 @@
+package com.paypal.psix;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by benny on 3/3/15.
+ */
+public class SetupEventActivity extends ActionBarActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_setup);
+    }
+
+}
