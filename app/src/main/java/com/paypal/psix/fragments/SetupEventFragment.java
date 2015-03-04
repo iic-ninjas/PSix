144e10812377ee6bab3528fdcdca516eecd5df0f
Benny Weingarten-Gabbay <gardenofwine@gmail.com>
2015-03-04 14:03:40 +0200

    Setup Event - use ButterKnife

--- app/src/main/java/com/paypal/psix/fragments/SetupEventFragment.java
+++ app/src/main/java/com/paypal/psix/fragments/SetupEventFragment.java
@@ -3,8 +3,6 @@ package com.paypal.psix.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,47 +11,42 @@ import android.widget.EditText;
 
 import com.paypal.psix.R;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnTextChanged;
+
 /**
  * Created by benny on 3/3/15.
  */
 public class SetupEventFragment extends Fragment {
 
+    @InjectView(R.id.edit_payment_sum) EditText paymentSumText;
+    @InjectView(R.id.edit_payment_reason) EditText paymentReasonText;
+    @InjectView(R.id.button_create_paymentLink) Button createPaymentLink;
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_setup_event, container);
-        addListeners(view);
+        ButterKnife.inject(this, view);
 
         return view;
     }
 
-    private void addListeners(View rootView) {
-        TextWatcher toggleCreateLinkButtonTextWatcher = createToggleCreateLinkButtonTextWatcher();
+    @OnTextChanged(R.id.edit_payment_reason)
+    public void paymentChanged(){
+        toggleCreateLinkButton();
+    }
 
-        final EditText paymentSumText = (EditText) rootView.findViewById(R.id.editPaymentSum);
-        paymentSumText.addTextChangedListener(toggleCreateLinkButtonTextWatcher);
-
-        final EditText paymentReasonText = (EditText) rootView.findViewById(R.id.editPaymentReason);
-        paymentReasonText.addTextChangedListener(toggleCreateLinkButtonTextWatcher);
+    @OnTextChanged(R.id.edit_payment_sum)
+    public void paymentReasonChanged(){
+        toggleCreateLinkButton();
     }
 
     private void toggleCreateLinkButton(){
-        Button createPaymentLink = (Button) getActivity().findViewById(R.id.buttonCreatePaymentLink);
-        createPaymentLink.setEnabled(isTextExist(R.id.editPaymentSum) && isTextExist(R.id.editPaymentReason));
+        createPaymentLink.setEnabled(doesTextExist(paymentReasonText) && doesTextExist(paymentSumText));
     }
 
-    private TextWatcher createToggleCreateLinkButtonTextWatcher(){
-        return new TextWatcher() {
-            public void afterTextChanged(Editable s) {}
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
-
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                toggleCreateLinkButton();
-            }
-        };
-    }
-
-    private boolean isTextExist(int id){
-        final EditText paymentSum = (EditText) getActivity().findViewById(id);
-        return paymentSum.getText().toString().length() > 0;
+    private boolean doesTextExist(EditText editText){
+        return editText.getText().toString().length() > 0;
     }
 }
