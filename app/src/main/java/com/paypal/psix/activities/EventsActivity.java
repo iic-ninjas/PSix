83e4f52411daef11b59ac72e99f0d194a5c04640
Shay Davidson <shay.h.davidson@gmail.com>
2015-03-04 10:37:19 +0200

    Removed main activity

--- app/src/main/java/com/paypal/psix/activities/EventsActivity.java
+++ app/src/main/java/com/paypal/psix/activities/EventsActivity.java
@@ -19,4 +19,26 @@ public class EventsActivity extends ActionBarActivity {
         getMenuInflater().inflate(R.menu.menu_events, menu);
         return true;
     }
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        int id = item.getItemId();
+//
+//        if (id == R.id.action_settings) {
+//            return true;
+//        } else if (id == R.id.action_debug_setup_event) {
+//            startActivity(new Intent(this, SetupEventActivity.class));
+//            return true;
+//        } else if (id == R.id.action_debug_events_list) {
+//            startActivity(new Intent(this, EventsActivity.class));
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 }
