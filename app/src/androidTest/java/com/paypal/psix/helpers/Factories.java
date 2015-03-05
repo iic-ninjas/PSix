60c1c1341d13c984eb92e78435037740fe87bf31
Avihu Turzion <avihu.turzion@gmail.com>
2015-03-05 16:00:24 +0200

    Expanded user name to first and last name

--- app/src/androidTest/java/com/paypal/psix/helpers/Factories.java
+++ app/src/androidTest/java/com/paypal/psix/helpers/Factories.java
@@ -13,13 +13,14 @@ public class Factories {
     public final static DataFactory rndData = new DataFactory();
 
     public static User createRandomUser() {
-        return createUser(rndData.getNumberText(FB_ID_LENGTH), rndData.getName());
+        return createUser(rndData.getNumberText(FB_ID_LENGTH), rndData.getName(), rndData.getName());
     }
 
-    public static User createUser(String fbUserId, String name) {
+    public static User createUser(String fbUserId, String firstName, String lastName) {
         User user = new User();
         user.fbUserId = fbUserId;
-        user.name = name;
+        user.firstName = firstName;
+        user.lastName = lastName;
         user.save();
         return user;
     }
