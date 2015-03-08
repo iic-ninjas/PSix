47c751ed6fc78896ef16ffdec5cfb3d3af69b242
Shay Davidson <shay.h.davidson@gmail.com>
2015-03-08 15:23:17 +0200

    onActivityResult

--- app/src/main/java/com/paypal/psix/activities/OnboardingActivity.java
+++ app/src/main/java/com/paypal/psix/activities/OnboardingActivity.java
@@ -2,63 +2,30 @@ package com.paypal.psix.activities;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.util.Log;
 
-import com.facebook.Session;
-import com.facebook.SessionState;
-import com.facebook.UiLifecycleHelper;
-import com.facebook.model.GraphUser;
-import com.facebook.widget.LoginButton;
-import com.paypal.psix.R;
+import com.paypal.psix.fragments.OnboardingFragment;
 
 
 public class OnboardingActivity extends FragmentActivity {
 
-    private static final String LOG_TAG = OnboardingActivity.class.getSimpleName();
-    private LoginButton loginButton;
-    private UiLifecycleHelper uiHelper;
-
-    private Session.StatusCallback statusCallback = new Session.StatusCallback() {
-        @Override
-        public void call(Session session, SessionState sessionState, Exception e) {
-            onSessionStateChange(session, sessionState, e);
-        }
-    };
+    private OnboardingFragment mainFragment;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        uiHelper = new UiLifecycleHelper(this, statusCallback);
-        uiHelper.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_onboarding);
-
-        loginButton = (LoginButton) findViewById(R.id.fb_login_button);
-        loginButton.setReadPermissions("public_profile", "user_events");
-        loginButton.setUserInfoChangedCallback(new LoginButton.UserInfoChangedCallback() {
-
-            @Override
-            public void onUserInfoFetched(GraphUser graphUser) {
-                Log.i(LOG_TAG, "user info fetched");
-                if (graphUser != null) {
-                    Log.i(LOG_TAG, "User logged in: " + graphUser.getFirstName() + " " + graphUser.getLastName());
-                }
-            }
-        });
+        if (savedInstanceState == null) {
+            // Add the fragment on initial activity setup
+            mainFragment = new OnboardingFragment();
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(android.R.id.content, mainFragment)
+                    .commit();
+        } else {
+            // Or set the fragment from restored state info
+            mainFragment = (OnboardingFragment) getSupportFragmentManager()
+                    .findFragmentById(android.R.id.content);
+        }
     }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        uiHelper.onResume();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        uiHelper.onPause();
-    }
-
-    private void onSessionStateChange(Session session, SessionState state, Exception e) {}
 
 }
--- app/src/main/java/com/paypal/psix/fragments/OnboardingFragment.java
+++ app/src/main/java/com/paypal/psix/fragments/OnboardingFragment.java
@@ -1,38 +1,38 @@
 package com.paypal.psix.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import com.facebook.Session;
-import com.facebook.SessionState;
-import com.facebook.UiLifecycleHelper;
+
 import com.facebook.widget.LoginButton;
 import com.paypal.psix.R;
-import butterknife.ButterKnife;
-import butterknife.InjectView;
+
+import java.util.Arrays;
 
 
 public class OnboardingFragment extends Fragment {
 
-    private static final String LOG_TAG = OnboardingFragment.class.getSimpleName();
-    private UiLifecycleHelper fbLifecycleHelper;
-    @InjectView(R.id.fb_login_button) LoginButton fbLoginButton;
 
-    public OnboardingFragment() {}
+    private static final String TAG = "MainFragment";
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
                              Bundle savedInstanceState) {
-        Log.i(LOG_TAG, "onCreateView");
-        View rootView = inflater.inflate(R.layout.fragment_onboarding, container, false);
-        ButterKnife.inject(this, rootView);
+        View view = inflater.inflate(R.layout.fragment_onboarding, container, false);
 
-        fbLoginButton.setReadPermissions("public_profile");
+        LoginButton authButton = (LoginButton) view.findViewById(R.id.authButton);
+        authButton.setReadPermissions(Arrays.asList("public_profile"));
+        authButton.setFragment(this);
 
-        return rootView;
+        return view;
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 }
