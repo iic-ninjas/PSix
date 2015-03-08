998de867198bb7f141299bf2329288523e8d9f7b
Shay Davidson <shay.h.davidson@gmail.com>
2015-03-08 15:46:34 +0200

    Splash

---
+++ app/src/main/java/com/paypal/psix/activities/Splash.java
@@ -0,0 +1,29 @@
+package com.paypal.psix.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.paypal.psix.R;
+
+public class Splash extends Activity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 2500;
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.splash);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(Splash.this, EventsActivity.class);
+                Splash.this.startActivity(mainIntent);
+                Splash.this.finish();
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
